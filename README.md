fa3b70b2eaca9d34333bc98954528d29d800baa1
Aaron Ladage <aladage@degdigital.com>
2015-09-08 00:39:46 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# tabs
+# Tabs
 A tabs plugin, written in ES6 module syntax.
+
+## Sample Usage
+``` javascript
+tabsInst = tabs(tabsEl, tabOptions);
+```
+
+## Settings
+``` javascript
+let tabOptions = {
+	'activeClass': 'is-active',
+	'triggerClass': 'tabs__trigger',
+	'sectionClass': 'tabs__section',
+	'targetName': 'data-target',
+	'indexName': 'data-index',
+	'useTriggerOrder': true,
+	'initialActiveSlideIndex': 0,
+	'onTabChange': null
+}
+```
+
+## Available Methods
+* addTriggerEl()
+* destroy()
+
+## Event Callbacks
+* onTabChange()
+
+## Revision History
+* **1.0.0:** First commit.
---
+++ tabs.js
@@ -0,0 +1,119 @@
+import domUtils from "domUtils";
+import objectUtils from "objectUtils";
+
+let tabs = function(element, options) {
+
+	let settings,
+		triggerEls = [],
+		sectionEls = [],
+		tabsDestroyed = true,
+		initialTabSet = false,
+		defaults = {
+			activeClass: 'is-active',
+			triggerClass: 'tabs__trigger',
+			sectionClass: 'tabs__section',
+			targetName: 'data-target',
+			indexName: 'data-index',
+			useTriggerOrder: true,
+			initialActiveSlideIndex: 0,
+			onTabChange: null
+		};
+
+	function init() {
+		if (element) {
+			settings = objectUtils.extend(this, defaults, options);
+			tabsDestroyed = false;
+			triggerEls = Array.prototype.slice.call(element.querySelectorAll('.' + settings.triggerClass));
+			triggerEls.forEach(function(triggerEl, index) {
+				addSection(triggerEl, index);
+				if (triggerEl.classList.contains(settings.activeClass)) {
+					initialTabSet = true;	
+				}
+			});
+			setInitialTab();
+			bindEvents();
+		}
+	};
+
+	function setInitialTab() {
+		if (!initialTabSet) {
+			[triggerEls[settings.initialActiveSlideIndex], sectionEls[settings.initialActiveSlideIndex]].forEach(function(el) {
+				el.classList.add(settings.activeClass);
+			});
+		}
+	};
+
+	function bindEvents() {
+		document.addEventListener('click', function(e) {
+			let clickedEl = domUtils.isDescendentByClass(settings.triggerClass, e.target);
+			if ((tabsDestroyed === false) && (clickedEl !== false)) {
+				e.preventDefault();
+				onTriggerClick(clickedEl);
+			}
+		});
+	};
+
+	function onTriggerClick(clickedEl) {
+		let clickedIndex = clickedEl.getAttribute(settings.indexName),
+			oldIndex = null;
+		triggerEls.forEach(function(trigger, index) {
+			if (trigger.classList.contains(settings.activeClass)) {
+				oldIndex = index;
+			}
+			[trigger, trigger.tabTargetEl].forEach(function(el) {
+				if (index !== parseInt(clickedIndex)) {
+					el.classList.remove(settings.activeClass);
+				} else {
+					el.classList.add(settings.activeClass);
+				}
+			});
+		});
+		if (settings.onTabChange !== null) {
+			settings.onTabChange({
+				'newTabIndex': parseInt(clickedIndex),
+				'oldTabIndex': oldIndex
+			});
+		}
+	};
+
+	function addTriggerEl(triggerEl) {
+		triggerEls.push(triggerEl);
+		addSection(triggerEl, triggerEls.length);
+	};
+
+	function addSection(triggerEl, index) {
+		let sectionEl;
+		if (settings.useTriggerOrder === true) {
+			sectionEls = Array.prototype.slice.call(element.querySelectorAll('.' + settings.sectionClass));
+			sectionEl = sectionEls[index];
+		} else {
+			sectionEl = element.querySelector(triggerEl.getAttribute(settings.targetName));
+			sectionEls.push(sectionEl);
+		}
+		triggerEls[index]['tabTargetEl'] = sectionEl;
+		triggerEl.setAttribute(settings.indexName, index);
+	};
+
+	function destroy() {
+		triggerEls.forEach(function(triggerEl) {
+			triggerEl.removeEventListener('click', onTriggerClick);
+			triggerEl.classList.remove(settings.activeClass);
+		});
+		sectionEls.forEach(function(sectionEl) {
+			sectionEl.classList.remove(settings.activeClass);
+		});
+		triggerEls = [];
+		sectionEls = [];
+		tabsDestroyed = true;
+	};
+
+	init();
+
+	return {
+		addTriggerEl: addTriggerEl,
+		destroy: destroy
+	};
+
+};
+
+export default tabs;
